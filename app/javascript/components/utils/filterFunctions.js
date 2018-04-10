434a243f755a02469159b3183297f19615243e23
github username <github email address>
2018-04-09 17:10:20 -0700

    cutting out some extra code in filterHolders

--- app/javascript/components/utils/filterFunctions.js
+++ app/javascript/components/utils/filterFunctions.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 
 //------------------Median Income to Median Home Value Comparison--------------------/
-export function filterHolderFinder(filterName, params, activeFilters) {
+export function filterHolderFinder(filterName, params) {
 
-    if (activeFilters !== undefined && activeFilters.includes(filterName)) {
+    if (params !== undefined) {
         let obj = params.filter(e => Object.keys(e)[0] === filterName)
         return Object.values(obj[0])[0]
     } else {
