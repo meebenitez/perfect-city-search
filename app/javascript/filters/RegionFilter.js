5a101eab5a7c34b9a1aa6637e68f76fd2ae5a7ee
github username <github email address>
2018-06-13 22:01:10 -0700

    adding dropdown div to region filter

--- app/javascript/filters/RegionFilter.js
+++ app/javascript/filters/RegionFilter.js
@@ -36,31 +36,24 @@ class RegionFilter extends React.Component {
 
         return (
             <Aux>
-                {this.props.isActive === true ? 
-                <div className="filter-div filter-on tooltip-top" data-tooltip="test test yoyo">
-                    <div className="filter-container">
-                        <div className="left-filter-col">
-                        <img src={require('../../assets/images/bluehouse.png')} className="filter-icon"/>&nbsp;Region<label htmlFor="Region"></label><br></br>
-                        <select defaultValue={Object.values(this.props.params.filter((filter) => {return Object.keys(filter)[0] === "RegionFilter"})[0])[0].split("&")[0].split("=").pop()} id= "RegionFilter" onChange={this.handleChange}>
-                            <option value="pacific_coast">Pacific Coast</option>
-                            <option value="mountain">Mountain</option>
-                            <option value="southwest">Southwest</option>
-                            <option value="heartland">Heartland</option>
-                            <option value="midwest">Midwest</option>
-                            <option value="southeast">Southeast</option>
-                            <option value="appalachian_highlands">Appaliachia</option>
-                            <option value="mid_atlantic">Mid-Atlantic</option>
-                            <option value="new_england">New England</option>
+                {this.props.isActive === true ?
+                    <div className="filter-div filter-on tooltip-top" data-tooltip="test test yoyo">
+                        <img src={require('../../assets/images/bluehouse.png')} className="filter-icon"/>&nbsp;Region<label onClick={this.handleClick}>x</label><label htmlFor="Region"></label>
+                        <div className="region-div">
+                            <select defaultValue={Object.values(this.props.params.filter((filter) => {return Object.keys(filter)[0] === "RegionFilter"})[0])[0].split("&")[0].split("=").pop()} id= "RegionFilter" onChange={this.handleChange}>
+                                <option value="pacific_coast">Pacific Coast</option>
+                                <option value="mountain">Mountain</option>
+                                <option value="southwest">Southwest</option>
+                                <option value="heartland">Heartland</option>
+                                <option value="midwest">Midwest</option>
+                                <option value="southeast">Southeast</option>
+                                <option value="appalachian_highlands">Appaliachia</option>
+                                <option value="mid_atlantic">Mid-Atlantic</option>
+                                <option value="new_england">New England</option>
                             </select>
                         </div>
-                        <div className="right-filter-col">
-                            <div className="center-x">
-                                <label onClick={this.handleClick}>x</label>
-                            </div>
-                        </div>
                     </div>
-                </div> :
-                <div className="filter-div filter-off" data-balloon="test test yoyo" data-balloon-pos="up" data-balloon-length="medium" onClick={this.handleClick}>
+                : <div className="filter-div filter-off" data-balloon="test test yoyo" data-balloon-pos="up" data-balloon-length="medium" onClick={this.handleClick}>
                     <img src={require('../../assets/images/greyhouse.png')} className="filter-icon"/>&nbsp;Region<label htmlFor="RegionFilter"></label><br></br>
                 </div> }
             </Aux>
