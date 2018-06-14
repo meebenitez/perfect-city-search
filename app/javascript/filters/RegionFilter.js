fedc01fee4a9b2b3eae52d5c34f67268f08eb38a
github username <github email address>
2018-06-14 14:59:33 -0700

    wip region filter popup

--- app/javascript/filters/RegionFilter.js
+++ app/javascript/filters/RegionFilter.js
@@ -1,20 +1,54 @@
+
 import React from 'react';
 import Aux from '../components/Aux'
 
 class RegionFilter extends React.Component {
     constructor(props){
         super(props)
-        this.state = {region: 'pacific_coast', open: false}
+        this.state = {region: 'pacific_coast', regionPopup: false}
         this.handleClick = this.handleClick.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.regionRef = null;
+
+        this.setRegionRef = element => {
+        this.regionRef = element;
+        };
+
+        //this.focusRegionRef = () => {
+        //    if (this.regionRef) this.regionRef.focus();
+        //}
     }
 
+ 
+    //componentDidMount() {
+        // autofocus the input on mount
+      //  this.focusRegionRef();
+      //}
 
     handleClick(){
         if ( this.props.isActive === false ) {
-            this.props.onFilterChange("RegionFilter", `[region]=${this.state.region}`, `region=${this.state.region}` )
+            if (this.state.regionPopup === false) {
+                this.setState({
+                    regionPopup: true
+                }, () => {
+                    this.props.onFilterChange("RegionFilter", `[region]=${this.state.region}`, `region=${this.state.region}` )
+                })
+            } else {
+                this.setState({
+                    regionPopup: true
+                })
+            }
         } else {
-            this.props.onFilterChange("RegionFilter", "")
+            if (this.state.regionPopup === false) {
+                this.setState({
+                    regionPopup: true
+                })
+            } else {
+                this.setState({
+                    regionPopup: true
+                })
+            }
+            
         }
 
     }
@@ -29,33 +63,36 @@ class RegionFilter extends React.Component {
             })
         
     }
+
+ 
     
 
     render(){    
-
-
+    
         return (
             <Aux>
-                {this.props.isActive === true ?
-                    <div className="filter-div filter-on tooltip-top" data-tooltip="test test yoyo">
-                        <img src={require('../../assets/images/bluehouse.png')} className="filter-icon"/>&nbsp;Region<label onClick={this.handleClick}>x</label><label htmlFor="Region"></label>
-                        <div className="region-div">
-                            <select defaultValue={Object.values(this.props.params.filter((filter) => {return Object.keys(filter)[0] === "RegionFilter"})[0])[0].split("&")[0].split("=").pop()} id= "RegionFilter" onChange={this.handleChange}>
-                                <option value="pacific_coast">Pacific Coast</option>
-                                <option value="mountain">Mountain</option>
-                                <option value="southwest">Southwest</option>
-                                <option value="heartland">Heartland</option>
-                                <option value="midwest">Midwest</option>
-                                <option value="southeast">Southeast</option>
-                                <option value="appalachian_highlands">Appaliachia</option>
-                                <option value="mid_atlantic">Mid-Atlantic</option>
-                                <option value="new_england">New England</option>
-                            </select>
-                        </div>
+                
+                    <div className={this.props.isActive === true ? "filter-div filter-on tooltip-top" : "filter-div filter-off" } data-tooltip="test test yoyo" onClick={this.handleClick}>
+                        {this.props.isActive === true ? <img src={require('../../assets/images/bluehouse.png')} className="filter-icon"/> : <img src={require('../../assets/images/greyhouse.png')} className="filter-icon"/>}&nbsp;Region<label onClick={this.handleClick}>x</label><label htmlFor="Region"></label>
+                            {this.state.regionPopup === true ? 
+                                <div className="region-div" ref="region-node">
+                                    <span className="bold">Regions:</span>
+                                    <br></br>
+                                    <select defaultValue={this.props.params.length > 0 ? Object.values(this.props.params.filter((filter) => {return Object.keys(filter)[0] === "RegionFilter"})[0])[0].split("&")[0].split("=").pop() : "pacific_coast" } id= "RegionFilter" onChange={this.handleChange}>
+                                        <option value="pacific_coast">Pacific Coast</option>
+                                        <option value="mountain">Mountain</option>
+                                        <option value="southwest">Southwest</option>
+                                        <option value="heartland">Heartland</option>
+                                        <option value="midwest">Midwest</option>
+                                        <option value="southeast">Southeast</option>
+                                        <option value="appalachian_highlands">Appaliachia</option>
+                                        <option value="mid_atlantic">Mid-Atlantic</option>
+                                        <option value="new_england">New England</option>
+                                    </select>
+                                </div>    
+                                 : null }                       
                     </div>
-                : <div className="filter-div filter-off" data-balloon="test test yoyo" data-balloon-pos="up" data-balloon-length="medium" onClick={this.handleClick}>
-                    <img src={require('../../assets/images/greyhouse.png')} className="filter-icon"/>&nbsp;Region<label htmlFor="RegionFilter"></label><br></br>
-                </div> }
+                
             </Aux>
             )
         }
@@ -65,34 +102,3 @@ class RegionFilter extends React.Component {
 }
 
 export default RegionFilter;
-
-
-
-
-
-//const RegionFilter = (props) => {
-//    return (
-//        <div className="filter-div">
-//            <label htmlFor="RegionFilter"><img src={require('../../assets/images/region.png')} className="stat-icon-sm"/> Region:</label>
-//        </div>
-//    )
-//}
-
-//export default RegionFilter;
-
-
-
-//<select value={props.value} defaultValue={props.filterHolder} id= "RegionFilter" onChange={(event) => props.onFilterChange(event)}>
-//<option value="">Deactivate</option>
-//<option value="[region]=pacific_coast">Pacific Coast</option>
-//<option value="[region]=mountain">Mountain</option>
-//<option value="[region]=southwest">Southwest</option>
-//<option value="[region]=heartland">Heartland</option>
-//<option value="[region]=midwest">Midwest</option>
-//<option value="[region]=southeast">Southeast</option>
-//<option value="[region]=appalachian_highlands">Appaliachia</option>
-//<option value="[region]=mid_atlantic">Mid-Atlantic</option>
-//<option value="[region]=new_england">New England</option>
-//</select>
-
-
