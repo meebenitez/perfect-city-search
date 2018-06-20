f4231c7bdd9f9a2605f3e61abafb03c5aa6d2123
github username <github email address>
2018-06-20 01:04:41 -0700

    making divs conditional to the new check in popularFilter

--- app/javascript/filters/PopularFilter.js
+++ app/javascript/filters/PopularFilter.js
@@ -29,11 +29,11 @@ class PopularFilter extends React.Component {
         return (
             <Aux>
                 <div className="filter-popup-parent">
-                    <div className={this.props.isActive ? "filter-div filter-on tooltip-top" : "filter-div filter-off" } data-tooltip="test test yoyo" onClick={this.handleClick}>
-                        {this.props.isActive ? 
+                    <div className={this.props.activeFilters.includes("PopularFilter") ? "filter-div filter-on tooltip-top" : "filter-div filter-off" } data-tooltip="test test yoyo" onClick={this.handleClick}>
+                        {this.props.activeFilters.includes("PopularFilter") ? 
                             <img src={require('../../assets/images/star.png')} className="filter-icon"/> 
                             : <img src={require('../../assets/images/greystar.png')} className="filter-icon"/>}
-                        {this.props.isActive === true ?
+                        {this.props.activeFilters.includes("PopularFilter") ?
                             <span>&nbsp;&nbsp;<span className="bold">Popular</span><span onClick={this.handleClear}>&nbsp;&nbsp;&nbsp;<img src={require('../../assets/images/xout2.png')} className="filter-icon-sm"/></span></span>
                             : <span>&nbsp;Popular</span>}<label htmlFor="popular"></label>
                     </div>                
