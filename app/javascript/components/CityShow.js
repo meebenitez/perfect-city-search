2ab394b31bb58498e1d67dd8e80b90aedd7dec25
github username <github email address>
2018-06-25 18:34:03 -0700

    WIP city show styling

--- app/javascript/components/CityShow.js
+++ app/javascript/components/CityShow.js
@@ -15,12 +15,10 @@ const CityShow = (props) => {
             <div className="city-popup">
                 <div className='city-popup-inner'>
                     <div className="row">
-
-                        <div className="col-xs-12 row zero-padding">
-                            <div className="row left-push">
-                                <div className="row">
-                                    <div className="col-xs-5 city-img-container">
-                                        <div className="bottom-right2">
+                        <div className="col-xs-12 left-push row">
+                            <div className="col-xs-5 zero-padding">
+                                <div className="col-xs-12 zero-padding">
+                                    <div className="bottom-right2">
                                         <HeartButtonLg
                                             currentUser={props.currentUser} 
                                             heartedCities={props.heartedCities} 
@@ -28,64 +26,62 @@ const CityShow = (props) => {
                                             heartClick={props.heartClick} 
                                             toggleAuthPopup={props.toggleAuthPopup} 
                                             city={props.city}/>
-                                        </div>
-                                        <img className="img-city-icon-lg" src={props.city.img} onError={(e)=>{e.target.src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/CheyenneWY_downtown.jpg/1024px-CheyenneWY_downtown.jpg'}} />
                                     </div>
-                                    <div className="cols-xs-7 city-stat-container">
-                                            <div className="top-right2" onClick={props.closePopup}><img src={require('../../assets/images/xout2.png')} className="filter-icon-sm"/> </div>
-                                            <h1>{props.city.name}, {props.city.long_state}</h1>
-                                            <br></br>
-                                            <span style={{fontWeight: "bold", fontSize: "17px", color: "red"}}>{props.city.county}</span>
-                                            <span style={{fontWeight: "bold", fontSize: "17px"}}>&nbsp;({formatRegion(props.city.region)})</span>
-                                            <br></br>
-                                            <span style={{color: 'gold', fontSize:'15px'}}>{ (props.city.popularity >= 25) ? "⭐ Popular City ⭐ " : null }</span>
-                                            <br></br>
-                                            <br></br>
-                                            <span style={{fontWeight: "bold"}}>Population: </span><span>{withCommas(props.city.pop_total)} residents</span>
-                                            <br></br>
-                                            <br></br>
-                                            <br></br>
-                                            <span style={{fontSize: "12px"}}>Photo Credit: 
-                                            <br></br>
-                                            <Link to={props.city.img_wiki_src} target="_blank">"{props.city.img_title}" by {props.city.img_artist}</Link> - {props.city.img_license}
-                                            </span>
-                                </div>        
-                                    </div>
-                                <div className="row left-push city-stats-full">
-                                    <div className="city-stats-col">
-                                        <span style={{fontWeight: "bold", textDecoration: 'underline', fontSize: "17px"}}>Resident Demographics</span>
-                                        <br></br>
-                                        <br></br>
-                                        <span>Median Age: <span style={{fontWeight: "bold"}}> {props.city.age_median}</span></span>
-                                        <br></br>
-                                        <br></br>
-                                        <span>Racial Diversity:</span>
-                                        <br></br>
-                                        <span style={{fontWeight: "bold"}}>{Math.floor(props.city.pop_white_perc)}% White, {Math.floor(props.city.pop_black_perc)}% Black, {Math.floor(props.city.pop_native_perc)}% Native American, {Math.floor(props.city.pop_asian_perc)}% Asian, {Math.floor(props.city.pop_pacific_perc)}% Pacific Islander, {Math.floor(props.city.pop_latin_hispanic_perc)}% Latin/Hispanic, {Math.floor(props.city.pop_other_race_perc)}% Other, {Math.floor(props.city.pop_mixed_race_perc)}% Mixed Race</span>
-                                        <br></br>
-                                        <br></br>
-                                        <span>Veteran Population:</span><span style={{fontWeight: "bold"}}> {props.city.vets_perc}%</span>
-                                        <br></br>
-                                        <br></br>
-                                        <span>Political Diversity</span>
-                                        <br></br>
-                                        <span>(2016 results for {props.city.county})</span>
-                                        <br></br>
-                                        <span style={{fontWeight: "bold"}}>{Math.floor(props.city.dem_vote_perc)}% Clinton, {Math.floor(props.city.gop_vote_perc)}% Trump, {Math.floor(props.city.ind_vote_perc)}% Independent</span>
-
-                                    </div>
+                                    <img className="img-city-icon-lg" src={props.city.img} onError={(e)=>{e.target.src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/CheyenneWY_downtown.jpg/1024px-CheyenneWY_downtown.jpg'}} />
+                                </div>
+                                <div className="col-xs-12 zero-padding">
+                                    <span style={{fontSize: "12px"}}>Credit: <Link to={props.city.img_wiki_src} target="_blank">"{props.city.img_title}" by {props.city.img_artist}</Link> - {props.city.img_license}
+                                    </span>
                                 </div>
                             </div>
-                           
-                        
-                        
-                        
-                                <div>
-                                    
-                                   
-                                    
-                                </div>
+                            <div className="col-xs-7 zero-padding">
+                                <div className="top-right2" onClick={props.closePopup}><img src={require('../../assets/images/xout2.png')} className="filter-icon-sm"/> </div>
+                                <div className="zero-padding">
+                                <h1>{props.city.name}, {props.city.short_state}</h1>
+                                <br></br>
+                                <span style={{fontWeight: "bold", fontSize: "17px", color: "red"}}>{props.city.county}</span>
+                                <span style={{fontWeight: "bold", fontSize: "17px"}}>&nbsp;({formatRegion(props.city.region)})</span>
+                                <br></br>
+                                <span style={{color: 'gold', fontSize:'15px'}}>{ (props.city.popularity >= 25) ? "⭐ Popular City ⭐ " : null }</span>
+                                <br></br>
+                                <br></br>
+                                <span style={{fontWeight: "bold"}}>Population: </span><span>{withCommas(props.city.pop_total)} residents</span>
+                                <br></br>
+                                <span style={{fontWeight: "bold"}}>Median Age: </span><span>{props.city.age_median}</span>
+                                <br></br>
+                                
+
+                                </div> 
+                            </div>
                         </div>
+                        <div className="col-xs-12 left-push zero-padding row">
+                        <br></br>
+                        <h2>More Stats</h2>
+                        <br></br>
+                        <span style={{fontWeight: "bold"}}>Racial Diversity:</span>
+                            <br></br>
+                            <div className="stat-border">{Math.floor(props.city.pop_black_perc)}% African-American</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_white_perc)}% Caucasian</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_native_perc)}% Native American</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_asian_perc)}% Asian</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_pacific_perc)}% Pacific Islander</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_latin_hispanic_perc)}% Latin/Hispanic</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_other_race_perc)}% Other Race</div>
+                            <div className="stat-border">{Math.floor(props.city.pop_mixed_race_perc)}% Mixed Race</div>
+                            <br></br>
+                            <br></br>
+                            <span style={{fontWeight: "bold"}}>Veteran Population: </span>{props.city.vets_perc}% (US avg. is 6%)
+                            <br></br>
+                            <br></br>
+                            <span style={{fontWeight: "bold"}}>2016 Presidential Election Votes for {props.city.county}</span>
+                            <br></br>
+                            <div className="stat-border red-background">{Math.floor(props.city.gop_vote_perc)}% Trump</div>
+                            <div className="stat-border blue-background">{Math.floor(props.city.dem_vote_perc)}% Clinton</div>
+                            <div className="stat-border grey-background">{Math.floor(props.city.ind_vote_perc)}% Independent</div>
+                            
+                        </div>
+
+    
                     </div>        
                 </div>
             </div>
