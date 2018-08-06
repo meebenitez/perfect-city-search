0976da30fbacc8c8cbe2bfa106dd4927907b9a57
github username <meebenitez@gmail.com>
2018-08-05 23:41:15 -0700

    WIP responsive header

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -10,10 +10,10 @@ const Header = (props) => {
                
             <div className="row nav">
                 
-                <div className="col-xs-7 header-left-padding">
+                <div className="col-xs-4 header-left-padding">
                     <NavLink to="/"><h1>CitySleuth USA&nbsp;<img src={require('../../assets/images/redglass.png')} className="logo-icon"/></h1></NavLink>
                 </div>
-                <div className="col-xs-5 text-right zero-padding">
+                <div className="col-xs-8 header-right-container zero-padding">
                     <Navbar toggleAuthPopup={props.toggleAuthPopup} currentUser={props.currentUser} initialFetch={props.initialFetch}/>
                 </div>
             </div>
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Aux from './Aux'
 import { NavLink } from 'react-router-dom';
 import Autosuggest from 'react-autosuggest';
+import MediaQuery from 'react-responsive'
 
 
 
@@ -10,23 +11,43 @@ const Navbar = (props) => {
     return (
         <Aux>
 
+
+        <MediaQuery minWidth={768}>
             <NavLink
-                to={`${window.location.hash}`}
-                className="navlink1 link-div"
-                activeClassName="navlink1active">Advertise With Us</NavLink>
+                    to={`${window.location.hash}`}
+                    onClick={props.toggleAuthPopup}
+                    className="navlink1 link-div link-box"
+                    activeClassName="navlink1active">{ props.currentUser ?<h5>{props.currentUser}&nbsp;</h5> : <h5>Sign up</h5> }</NavLink>
             <NavLink
-                to={`${window.location.hash}`}
-                className="navlink1 link-div"
-                activeClassName="navlink1active">Faqs</NavLink>
-            <NavLink
-                to={`${window.location.hash}`}
-                className="navlink1 link-div"
-                activeClassName="navlink1active">Developer Blog</NavLink>
-            <NavLink
-              to={`${window.location.hash}`}
-              onClick={props.toggleAuthPopup}
-              className="navlink1 link-box"
-              activeClassName="navlink1active">{ props.currentUser ?<h5>{props.currentUser}&nbsp;</h5> : <h5>Sign up</h5> }</NavLink>
+                    to={`${window.location.hash}`}
+                    className="navlink1 link-div"
+                    activeClassName="navlink1active">Advertise With Us</NavLink>
+                <NavLink
+                    to={`${window.location.hash}`}
+                    className="navlink1 link-div"
+                    activeClassName="navlink1active">Faqs</NavLink> 
+        </MediaQuery>
+        <MediaQuery maxWidth={768}>
+            <div class="dropdown">
+                <button class="dropbtn navlink1 link-div">☰</button>
+                <NavLink
+                        to={`${window.location.hash}`}
+                        onClick={props.toggleAuthPopup}
+                        className="navlink1 link-div link-box"
+                        activeClassName="navlink1active">{ props.currentUser ?<h5>{props.currentUser}&nbsp;</h5> : <h5>Sign up</h5> }</NavLink>
+                <div class="dropdown-content">
+                    <a href={window.location.hash}>Advertise with Us</a>
+                    <a href={window.location.hash}>Faqs</a>
+                    <a href={window.location.hash}>Developer Blog</a>
+                    <a href={window.location.hash} onClick={props.toggleAuthPopup}>Sign out</a>
+
+                </div>
+            </div>
+
+            
+        </MediaQuery>
+
+           
               
       </Aux>
     )
