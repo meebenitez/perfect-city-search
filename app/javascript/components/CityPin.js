30deece04c73288576bbc9f548ab84a43ab1dfb8
github username <meebenitez@gmail.com>
2018-08-16 20:22:59 -0700

    WIP map names on highlight

--- app/javascript/components/CityPin.js
+++ app/javascript/components/CityPin.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import Aux from './Aux'
 
 
 const CityPin = props => {
 
     const renderPin = function(){
         if (props.heartedCities.find(function(city) {return city.id === props.city.id})) {
-            return <img src={require("../../assets/images/red_heart.png")} className="origin-zero"/>
+            return <div className="city-pin-container"><img src={require("../../assets/images/red_heart.png")} className="origin-zero"/></div>
         } else if (props.highlightedCity === props.city.id) {
-            return <img src={require("../../assets/images/red_city_pin.png")} className="origin-zero highlight"/>
+            return <div className="city-pin-container"><img src={require("../../assets/images/red_city_pin.png")} className="origin-zero highlight"/><div className="city-pin-popup highlight">{props.city.name}</div></div>
         } else {
-            return <img src={require("../../assets/images/green_city_pin.png")} className="origin-zero"/>
+            return <div className="city-pin-container"><img src={require("../../assets/images/green_city_pin.png")} className="origin-zero"/></div>
         }
     }
 
     return (
-            <div>
+            <Aux>
                 {renderPin()}
-            </div>
+            </Aux>
 
         
         
