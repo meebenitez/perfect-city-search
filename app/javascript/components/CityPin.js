2c417e68fdf44451820780e1448429b6301bd0e8
github username <meebenitez@gmail.com>
2018-08-19 13:42:15 -0700

    removed heart icons from map

--- app/javascript/components/CityPin.js
+++ app/javascript/components/CityPin.js
@@ -8,9 +8,7 @@ import { Route, Link } from 'react-router-dom';
 const CityPin = props => {
 
     const renderPin = function(){
-        if (props.heartedCities.find(function(city) {return city.id === props.city.id})) {
-            return <div className="city-pin-container"><img src={require("../../assets/images/red_heart.png")} className="origin-zero"/></div>
-        } else if (props.highlightedCity === props.city.id) {
+        if (props.highlightedCity === props.city.id) {
             return <div className="city-pin-container"><img src={require("../../assets/images/red_city_pin.png")} className="origin-zero highlight"/><div className="city-pin-popup highlight"><Link to={`#city=${props.city.name}_${props.city.short_state}_${props.city.id}`} onClick= {() => props.showSingleCity()} >{props.city.name}, {props.city.short_state}</Link>{ (props.city.popularity >= 25) ? <span className="star">&nbsp;&nbsp;★</span> : null }</div></div>
         } else {
             return <div className="city-pin-container"><img src={require("../../assets/images/green_city_pin.png")} className="origin-zero"/></div>
