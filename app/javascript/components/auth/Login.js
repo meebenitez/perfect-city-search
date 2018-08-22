410e34a497ebaa772f4a6e8276bc3cea13e0145f
github username <meebenitez@gmail.com>
2018-08-21 18:13:17 -0700

    posting recruiter login info

--- app/javascript/components/auth/Login.js
+++ app/javascript/components/auth/Login.js
@@ -20,10 +20,13 @@ class Login extends React.Component {
             <div> 
                 <h3>Login</h3>
                 <br></br>
+                (Recruiter login: test@email.com, password1234)
+                <br></br>
                 <form>
                     <input id="email" placeholder="email" /><br></br>
                     <input id="password" type="password"placeholder="password" /><br></br>
                     <button onClick={e => this.handleLogin(e)}>Submit</button>
+                    <br></br>
                 </form>
             </div>
         ) 
